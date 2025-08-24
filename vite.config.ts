e56bcc102fd5505cfff0f1e695d6ccf7ada59da3
NRDEV <nrdev@outlook.com.br>
2025-08-24 15:56:02 -0300

    fix errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ].filter(Boolean),
+export default defineConfig(({
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    rollupOptions: {
-      input: "./index.html", // Caminho relativo simples
     },
   },
 }));
